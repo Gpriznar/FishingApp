71dbe51b13a7f8b8240756f2adc9fd807cc2b0e9
Tim Goens <timcgoens@gmail.com>
2020-08-03 11:45:28 -0500

    changed the forcast style to look better

--- FishingApp/Client/src/components/Login.js
+++ FishingApp/Client/src/components/Login.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react'
-import {Registration} from './Registration'
+import React, { Component } from 'react'
+// import {Registration} from './Registration'
 import axios from 'axios'
 import { connect } from 'react-redux'
 import { setAuthenticationHeader } from '../utils/authenticate'
@@ -8,52 +8,52 @@ import './Login.css';
 
 class Login extends Component {
 
-    constructor() {
-      super()
+  constructor() {
+    super()
 
-      this.state = {
-        username: '',
-        password: '',
-        message: ''
-      }
+    this.state = {
+      username: '',
+      password: '',
+      message: ''
     }
+  }
 
-    handleLoginClick = () => {
+  handleLoginClick = () => {
 
-    axios.post('https://fishing-app-backend-server.herokuapp.com/login',{
-    username: this.state.username,
-    password: this.state.password
-  }).then(response => {
+    axios.post('https://fishing-app-backend-server.herokuapp.com/login', {
+      username: this.state.username,
+      password: this.state.password
+    }).then(response => {
 
-    let token = response.data.token
-    let id = response.data.id
+      let token = response.data.token
+      let id = response.data.id
 
-    localStorage.setItem('jsonwebtoken',token)
-    localStorage.setItem('userId', id)
-    this.props.onAuthenticated(token,id)
-    setAuthenticationHeader(token)
-  }).catch(error => console.log(error))
+      localStorage.setItem('jsonwebtoken', token)
+      localStorage.setItem('userId', id)
+      this.props.onAuthenticated(token, id)
+      setAuthenticationHeader(token)
+    }).catch(error => console.log(error))
   }
 
 
-    handleTextBoxChange = (e) => {
-      this.setState({
-        [e.target.name]: e.target.value
-      })
-    }
+  handleTextBoxChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
 
   render() {
-    return(
+    return (
       <div className="loginbox">
-      <h1> Welcome to Fishing Journal </h1>
-      <h2>Login</h2>
-      <p>If this is your first time using Fishing App please register before attempting to login</p>
-          <div className="logininputsbox">
-        <input className="logininputfield" name="username" onChange={this.handleTextBoxChange} placeholder="User Name"></input>
-        <input className="logininputfield" name="password" onChange={this.handleTextBoxChange} placeholder="Password"></input>
-        <button className="loginbutton" onClick={this.handleLoginClick}>Login</button>
-        <label>{this.state.message}</label>
-          </div>
+        <h1> Welcome to Fishing Journal </h1>
+        <h2>Login</h2>
+        <p>If this is your first time using Fishing App please register before attempting to login</p>
+        <div className="logininputsbox">
+          <input className="logininputfield" name="username" onChange={this.handleTextBoxChange} placeholder="User Name"></input>
+          <input className="logininputfield" name="password" onChange={this.handleTextBoxChange} placeholder="Password"></input>
+          <button className="loginbutton" onClick={this.handleLoginClick}>Login</button>
+          <label>{this.state.message}</label>
+        </div>
       </div>
     )
   }
@@ -61,7 +61,7 @@ class Login extends Component {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    onAuthenticated: (token, id) => dispatch({type: 'ON_AUTHENTICATED', token:token, id:id})
+    onAuthenticated: (token, id) => dispatch({ type: 'ON_AUTHENTICATED', token: token, id: id })
   }
 }
 
--- FishingApp/Client/src/index.js
+++ FishingApp/Client/src/index.js
@@ -5,12 +5,12 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux'
 import reducer from './store/reducer'
-import requireAuth from './components/requireAuth'
-import {Provider} from 'react-redux'
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+// import requireAuth from './components/requireAuth'
+import { Provider } from 'react-redux'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import AddNewFish from './components/AddNewFish'
 import BaseLayout from './components/BaseLayout'
-import Login from './components/Login'
+// import Login from './components/Login'
 import Weather from './components/Weather'
 import PreviousFishList from './components/PreviousFishList'
 import Map from './components/Map'
@@ -19,19 +19,19 @@ const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window
 
 ReactDOM.render(
   <Provider store={store}>
-  <BrowserRouter>
-  <BaseLayout>
-    <Switch>
-      <Route exact path ='/' component={App} />
-      <Route path ='/addnewfish' component={AddNewFish} />
-      <Route path ='/previousfishlist' component={PreviousFishList} />
-      <Route path ='/weather' component={Weather} />
-      <Route path ='/heatmap' component={Map} />
-    </Switch>
-    </BaseLayout>
+    <BrowserRouter>
+      <BaseLayout>
+        <Switch>
+          <Route exact path='/' component={App} />
+          <Route path='/addnewfish' component={AddNewFish} />
+          <Route path='/previousfishlist' component={PreviousFishList} />
+          <Route path='/weather' component={Weather} />
+          <Route path='/heatmap' component={Map} />
+        </Switch>
+      </BaseLayout>
     </BrowserRouter>
   </Provider>
-      , document.getElementById('root'));
+  , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
