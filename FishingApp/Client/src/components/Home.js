a6e85c04e1efd5e4586e3b13fca91b8f80d2d00b
Tim Goens <timcgoens@gmail.com>
2020-08-04 14:07:28 -0500

    fixed embedded tweets

--- FishingApp/Client/src/components/Home.js
+++ FishingApp/Client/src/components/Home.js
@@ -1,27 +1,31 @@
 import React from 'react';
 import { TwitterTimelineEmbed } from 'react-twitter-embed';
+import './Home.css';
 
 
 const Home = () => {
 
     return (
-        <div>
+        <div className='home-page'>
             <h1>Fishing Journal</h1>
-          <div className="centerContent">
-          <div className="selfCenter standardWidth">
-          <TwitterTimelineEmbed sourceType="profile" userId={1071567792} options={{height: 400}} />
-          </div>
-          </div>
-          <div className="centerContent">
-          <div className="selfCenter standardWidth">
-          <TwitterTimelineEmbed sourceType="profile" userId={66430922} options={{height: 400}} />
-          </div>
-          </div>
-          <div className="centerContent">
-          <div className="selfCenter standardWidth">
-          <TwitterTimelineEmbed sourceType="profile" userId={21713042} options={{height: 400}} />
-          </div>
-          </div>
+            <div className='tweet-flex'>
+                <div className="embeded-tweet">
+                    <div className="selfCenter standardWidth">
+                        <TwitterTimelineEmbed sourceType="profile" userId={1071567792} options={{ height: 400 }} />
+                    </div>
+                </div>
+                <div className="embeded-tweet">
+                    <div className="selfCenter standardWidth">
+                        <TwitterTimelineEmbed sourceType="profile" userId={66430922} options={{ height: 400 }} />
+                    </div>
+                </div>
+                <div className="embeded-tweet">
+                    <div className="selfCenter standardWidth">
+                        <TwitterTimelineEmbed sourceType="profile" userId={21713042} options={{ height: 400 }} />
+                    </div>
+                </div>
+            </div>
+
         </div>
 
 
