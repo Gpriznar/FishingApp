00c55f156a4cca4f2f860dcc4eca223927e24b7d
Gregory Priznar <gpriznar@gmail.com>
2020-08-05 10:45:53 -0500

    added build

---
+++ FishingApp/Client/build/precache-manifest.7a6275bbd51e66e886152d4e9fab81e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cd63a1b334074ea318be474a033a390",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4260e4346634e603ba40",
+    "url": "/static/css/main.583b0207.chunk.css"
+  },
+  {
+    "revision": "51bce658d327e2964dec",
+    "url": "/static/js/2.004360d0.chunk.js"
+  },
+  {
+    "revision": "4260e4346634e603ba40",
+    "url": "/static/js/main.b214b903.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
