a0f23ea03596ae8a1680f9e35328bb40616a9cdf
Gregory Priznar <gpriznar@gmail.com>
2020-08-05 10:42:40 -0500

    gave site description it's own class name

--- FishingApp/Client/src/components/Home.js
+++ FishingApp/Client/src/components/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
         <div className='home-page'>
             <h1>Fishing Journal</h1>
             <div className='description'>
-                <p> Fishing Journal is an application designed to help fisherman track their catches using geolocation data. Catalog and view your caught fish using our "Add Fish" and "View All Fish" features. Make sure to utilize the Weather search to get a three day forecast so you can better plan your fishing trip!</p>
+                <p className="descriptionText"> Fishing Journal is an application designed to help fisherman track their catches using geolocation data. Catalog and view your caught fish using our "Add Fish" and "View All Fish" features. Make sure to utilize the Weather search to get a three day forecast so you can better plan your fishing trip!</p>
             </div>
             <div className='tweet-flex'>
                 <div className="embeded-tweet">
